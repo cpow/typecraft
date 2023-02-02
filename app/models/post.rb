6f92c63f8566e216fcb9ea01f0e32a9768243ab7
Christopher Power <chris.power@underdogfantasy.com>
2023-02-02 10:11:50 -0500

    add image_processing and model annotation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  title      :string
+#  body       :text
+#  blurb      :text
+#  iframe     :text
+#  technology :string
+#
 class Post < ApplicationRecord
   enum technology: {
     ruby: "ruby",
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  title      :string
+#  body       :text
+#  blurb      :text
+#  iframe     :text
+#  technology :string
+#
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
